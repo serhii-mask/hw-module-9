43c3191e147bcec3f193540ed522e4e7a70e2710
Serhii <serhii.mask@gmail.com>
2023-05-20 20:45:20 +0300

    fix: add two element for myLinkedList

--- src/main/java/myLinkedList/MyLinkedListTests.java
+++ src/main/java/myLinkedList/MyLinkedListTests.java
@@ -7,6 +7,8 @@ public class MyLinkedListTests {
         list.add("First");
         list.add("Second");
         list.add("Third");
+        list.add("Fourth");
+        list.add("Fifth");
         System.out.println("list.get(0) = " + list.get(0));
         System.out.println("list.get(1) = " + list.get(1));
         System.out.println("list.get(2) = " + list.get(2));
